859d601f75f41f50eb88e1a3bb13f9d36ecb5d62
Ktrips <kenichi_yoshida@hotmail.com>
2019-05-16 02:02:16 +0900

    Update button_vision.sh

--- button_vision.sh
+++ button_vision.sh
@@ -7,7 +7,7 @@ export GOOGLE_APPLICATION_CREDENTIALS=/home/pi/vision.json
 echo $GOOGLE_APPLICATION_CREDENTIALS
 #amixer --card 1 cset numid=11 100%
 
-cd /home/RasAi/Programs
+cd /home/pi/RasAi/
 echo "Running button_vision.py!"
 python button_vision.py
 echo "Vision is done!"
