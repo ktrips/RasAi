a216cbfb184558d9667fef803a6bfcf76841c11c
Ktrips <kenichi_yoshida@hotmail.com>
2019-08-18 12:26:11 +0900

    Update streaming_minutes.py

--- Programs/minutes/streaming_minutes.py
+++ Programs/minutes/streaming_minutes.py
@@ -19,3 +19,29 @@
        minutes+= minutes_temp["contents"] + '\n' + conv_cont
        subject = conv_title + '(' + dateStr + ')'
        return subject, minutes
+
+      
+       
+# (def mainプログラム中)
+           bye_words    = ['終わりです', '終了', 'おわり', 'さようなら', 'さよなら', 'バイバイ']
+…
+               aiy.audio.say(nolang, "en-US")
+           if text in bye_words:
+             subject, minutes = make_minutes(convs)
+             if mail:
+               os.system('echo "' + minutes + '" | mail -s "' + subject + '" ' + mail)
+             break
+           time.sleep(0.2)
+
+
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--speech', dest='speech_lang', type=str, default='', help='Base speech language')
+   parser.add_argument('--repeat', dest='repeat', type=str, default='speech', help='Repeat the speech or not')
+   parser.add_argument('--mail', dest='mail', type=str, default='', help='send to email')
+   args   = parser.parse_args()
+   speech_lang= args.speech_lang if args.speech_lang else default_speech
+   repeat = args.repeat if args.repeat else "speech"
+   mail   = args.mail if args.mail else ''
+   main()
